1e6de9ae4582c3e0fb0b50eb19cd8329105f3e54
Kurt HDz <julianhernandez.bohorquez@gmail.com>
2024-06-04 16:18:44 -0500

    Actuaizacion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,9 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        $user = $this->getUser();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'HomeController' ,'user' => $user
         ]);
     }
 }
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Services\ProfileService;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -27,73 +28,18 @@ class ProfileController extends AbstractController
     #[Route('/change/password', name: 'change_password', methods: ['POST'])]
     public function change_password(
         Request $request,
-        UserRepository $userRepository,
-        UserPasswordHasherInterface $userPasswordHasher,
-        MessagesController $messagesController
+        ProfileService $profileService
     ): JsonResponse {
-        $password = $request->request->get('newpassword');
-        $newpassword = $request->request->get('renewpassword');
-        $json = [];
 
-        if ($password == "" || $newpassword == "") {
-            $json = ['message' => $messagesController->space_in_blank()];
-            return new JsonResponse($json, 200, ['Content-Type' => 'application/json']);
-        }
-
-        if ($password != $newpassword) {
-            $json = ['message' => $messagesController->password_no_match()];
-            return new JsonResponse($json, 200, ['Content-Type' => 'application/json']);
-        }
-
-        if ($password === $newpassword) {
-            $user = $userRepository->find($this->getUser());
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $newpassword
-                )
-            );
-            $userRepository->Save($user);
-            $json = ['message' => $messagesController->password_change_completed()];
-            return new JsonResponse($json, 200, ['Content-Type' => 'application/json']);
-        }
-        return new JsonResponse($json, 200, ['Content-Type' => 'application/json']);
+        return $profileService->actualizarPassword($request, $this->getUser());
     }
 
     #[Route('/change/profile', name: 'change_profile', methods: ['POST'])]
     public function profile(
         Request $request,
-        UserRepository $userRepository,
-        MessagesController $messagesController
+        ProfileService $profileService
+
     ): JsonResponse {
-        $user = $this->getUser();
-
-        $fullName = $request->request->get('fullName');
-        $company = $request->request->get('company');
-        $job = $request->request->get('job');
-        $email = $request->request->get('email');
-
-        $json = [];
-
-        if (empty($fullName) || empty($company) || empty($job) || empty($email)) {
-            $json = ['message' => $messagesController->space_in_blank()];
-            return new JsonResponse($json, 200, ['Content-Type' => 'application/json']);
-        }
-
-        $existingUser = $userRepository->findOneBy(['email' => $email]);
-        if ($existingUser && $existingUser !== $user) {
-            $json = ['message' => $messagesController->email_already_exists()];
-            return new JsonResponse($json, 200, ['Content-Type' => 'application/json']);
-        }
-
-        $usuario = $userRepository->find($this->getUser());
-        $usuario->setName($fullName);
-        $usuario->setBussnies($company);
-        $usuario->setJob($job);
-        $usuario->setEmail($email);
-        $userRepository->save($usuario);
-
-        $json = ['message' => $messagesController->profile_update_completed()];
-        return new JsonResponse($json, 200, ['Content-Type' => 'application/json']);
+        return $profileService->UpdateProfile($request, $this->getUser());
     }
 }
